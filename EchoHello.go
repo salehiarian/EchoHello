ce1d00c4a7d8764fcfbbc0f0103a59975204055d
Arian Salehi Abari <arian99@my.yorku.ca>
2022-05-26 15:42:09 -0400

    initial commit

--- EchoHello.go
+++ EchoHello.go
@@ -5,11 +5,11 @@ import (
 	"net/http"
 )
 
+func generateHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+}
+
 func main() {
 	http.HandleFunc("/", generateHello)
 	http.ListenAndServe(":8080", nil)
-}
-
-func generateHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 }
